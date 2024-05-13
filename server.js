030bcf3afcc9b6913c87d9d44bff5fa8e95c1f32
Micah Embree <mdembree@ncsu.edu>
2024-05-13 17:52:44 -0400

    chore: Refactor server.js and update file paths for generated PDFs

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express'); // Import our Express dependency
 const multer = require('multer');
 const fs = require('fs');
-const app = express(); // Create an Express application
 const path = require('path'); // Import the path module
 const spawn = require('child_process').spawn;
+const { exec } = require("child_process");
+
+const app = express(); // Create an Express application
 const PORT = 5000; // Port number we want to use of this server
 
 const html_path = path.join(__dirname, 'templates'); // HTML files folder
@@ -18,6 +20,8 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage }).single('chordproFile');
 
+let outputFileName; // Declare outputFileName as a global variable
+
 // Route for serving the conversion form
 app.get('/', (req, res) => {
     res.sendFile(path.join(html_path, 'ccpdfgenerator.html')); // Update the file path here
@@ -30,7 +34,7 @@ app.post('/submit', upload, (req, res) => {
         console.log(req.file.path);
 
         const cpnicepdfPath = '/usr/local/bin/cpnicepdf';
-        const { transposeValue, outputFileName } = req.body;
+        const { transposeValue } = req.body;
         const chordproFilePath = req.file.path;
 
         if (req.file === '' || transposeValue === '' || outputFileName === '') {
@@ -43,12 +47,13 @@ app.post('/submit', upload, (req, res) => {
             throw new Error('Transpose value must be between -12 and 12');
         }
 
-        const { exec } = require("child_process");
-        const command = `${cpnicepdfPath} ${chordproFilePath} generated-chord-charts/${outputFileName}.pdf ${transposeValue} chordpro.json`;
+        outputFileName = req.body.outputFileName; // Assign the value to the global variable
+
+        const command = `${cpnicepdfPath} ${chordproFilePath} /app/generated-chord-charts/${outputFileName}.pdf ${transposeValue} chordpro.json`;
         console.log(command);
         exec(command, (error, stdout, stderr) => {
-            if (error !== null) {
-                console.log("error: " + error);
+            if (stderr !== null) {
+                console.log("error: " + stderr);
             } else {
                 console.log("success: " + stdout);
             }
@@ -77,10 +82,10 @@ app.get('/static/css/ccpdfgenerator.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'css', 'ccpdfgenerator.css'));
 });
 
-// Set the MIME type for CSS files
-app.get('/static/css/result.css', (req, res) => {
-    res.setHeader('Content-Type', 'text/css');
-    res.sendFile(path.join(__dirname, 'static', 'css', 'result.css'));
+// Set the MIME type for PDF files
+app.get('/download', (req, res) => {
+    res.setHeader('Content-Type', 'application/pdf');
+    res.download(path.join(__dirname, 'generated-chord-charts', `${outputFileName}.pdf`));
 });
 
 // Start the server
